b91824035ae3ab6e00b5c4f383a388a9ac0634d4
Robert Durfee <robert.durfee.17@gmail.com>
2020-06-12 07:32:14 -0500

    unexport macro

--- src/directed_graph.rs
+++ src/directed_graph.rs
@@ -3,17 +3,6 @@ use std::collections::HashSet as Set;
 use std::hash::Hash;
 use std::rc::Rc;
 
-#[macro_export]
-macro_rules! map {
-    ($($x:expr => $y:expr),*) => {{
-        #[allow(unused_mut)]
-        let mut temp_map = std::collections::HashMap::new();
-        $(temp_map.insert($x, $y);)*
-        temp_map
-    }}
-}
-
-#[macro_export]
 macro_rules! set {
     ($($x:expr),*) => {{
         #[allow(unused_mut)]
